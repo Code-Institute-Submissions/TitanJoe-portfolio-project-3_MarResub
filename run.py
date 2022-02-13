873476252e35f473bb4f7f35521f10e00ffe54a7
Joseph Devaney <Joethesheepfarmer@gmail.com>
2022-02-13 15:27:18 +0000

    add pop_board function

--- run.py
+++ run.py
@@ -20,6 +20,21 @@ class Board:
         ['. ', '. ', '. ', '. '],
         ['. ', '. ', '. ', '. ']]
 
+    def pop_board(self):
+        """
+        this is a doc string
+        """
+        while len(self.loc_ships) < 4:
+            list = []
+            while len(list) < 2:
+                point = randint(0,3)
+                list.append(point)
+            if list not in self.loc_ships: 
+                self.loc_ships.append(list)
+                coord_x = list[0]
+                coord_y = list[1]
+                self.your_board[coord_x][coord_y] = "@ "
+
 def new_game():
     """
     this is a doc string
@@ -30,5 +45,8 @@ def new_game():
 
     computer_board = Board("enemy", "computer")
     player_board = Board(cptn_name, "player")
+
+    computer_board.pop_board()
+    player_board.pop_board()
 
 new_game()
