5711d2904c35aba4d2d4b430c00f8c06630ecaad
Joseph Devaney <Joethesheepfarmer@gmail.com>
2022-02-13 15:24:50 +0000

    add board class and new game function

--- run.py
+++ run.py
@@ -1,4 +1,34 @@
+from random import randint
 
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board:
+    """
+     
+    """
+    def __init__(self, cptn, player_type):
+        self.cptn = cptn
+        self.player_type = player_type
+        self.loc_ships = []
+        self.your_board = [
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. ']]
+        self.your_hidden_board = [
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. '],
+        ['. ', '. ', '. ', '. ']]
 
-print("Welcome to BATTLESHIPS!")
+def new_game():
+    """
+    this is a doc string
+    """
+    print("Welcome to Battleships")
+    cptn_name = input("What is your name: ")
+    print(f"Welcome Captian {cptn_name}.")
+
+    computer_board = Board("enemy", "computer")
+    player_board = Board(cptn_name, "player")
+
+new_game()
